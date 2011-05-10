4ab280a1f58574a2ca2bb59764e7315bc9450fcd
William Heymann <kosh@aesaeion.com>
2011-05-10 13:32:53 -0600

    upgraded to a new version of jquery ui

--- com/css.py
+++ com/css.py
@@ -16,5 +16,5 @@ def default_css(enable_gzip, jquery_ui_theme):
     """
     gz = '.gz' if enable_gzip else ''
     jquery_ui_theme = jquery_ui_theme if jquery_ui_theme in allowed_themes else 'base'
-    return '''<link rel="stylesheet" type="text/css" href="http://ajax.googleapis.com/ajax/libs/jqueryui/1.8.11/themes/{theme}/jquery-ui.css">
+    return '''<link rel="stylesheet" type="text/css" href="http://ajax.googleapis.com/ajax/libs/jqueryui/1.8.12/themes/{theme}/jquery-ui.css">
         <link rel="stylesheet" type="text/css" href="http://s3.amazonaws.com/media.webmediaengineering.com/CompoundDoc/default_9{gz}.css">'''.format(gz=gz, theme=jquery_ui_theme)
